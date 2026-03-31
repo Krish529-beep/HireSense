50c58ec7cba16aeadac3804429267653b46d31ff
Krish <igc394777@gmail.com>
2026-04-01 01:23:21 +0530

    added auth apis

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 // app.js used for server intiate , middlewares , routes handling
 
 const express = require('express');
-
 const app = express()
+const cookieParser = require('cookie-parser')
 
 app.use(express.json())
+app.use(cookieParser())
 
+// Require all the routes here 
+const authRouter = require('./routes/auth.route.js')
+
+// Using all the routes here
+// Learing: Give path correctly
+app.use("/api/auth",authRouter)
 
 module.exports = app
