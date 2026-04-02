c11b47961f39ae0485179ab972dc068a95ede20f
Krish <igc394777@gmail.com>
2026-04-02 22:06:34 +0530

    api setup and authprovider

---
+++ frontend/src/features/auth/auth.context.jsx
@@ -0,0 +1,19 @@
+import {createContext,useState} from 'react'     
+
+
+
+
+export const AuthContext = createContext()
+
+
+
+export const AuthProvider = ({children}) => {
+    const [user,setUser] = useState(null)
+    const [lodaing,setLoading] = useState(false) // in production it is set to true
+     
+    return(
+        <AuthContext.Provider value={{user,setUser,lodaing,setLoading}} >
+            {children}
+        </AuthContext.Provider>
+    )
+}
